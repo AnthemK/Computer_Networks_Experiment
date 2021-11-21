e01cfe43eafd3586539881e9412a7f5dbbc75f00
anthemk <lwz0920kk@gmail.com>
2021-11-22 00:09:02 +0800

    2021/11/22 0：08

--- Socket_Programming/Client.cpp
+++ Socket_Programming/Client.cpp
@@ -33,81 +33,55 @@ namespace TFTP {
 			Log_Output::OutputtoBoth(1, "Send Packet Error!");
 			return -1;
 		}
-		while (1) {
-			while (true) {
-				err = RecivePacket();	//进行一次接收
-				if (err <= 0) {
-					if (time(nullptr) >= Connection_Infor.ResendTimer && Connection_Infor.RemainResendNum <= 0) {  //超时并且重传次数用光了
-						Log_Output::OutputtoBoth(1, "Can not Recieve Response for packet!");
+		while (true) {
+			err = RecivePacket();	//进行一次接收
+			if (err <= 0) {
+				if (time(nullptr) >= Connection_Infor.ResendTimer && Connection_Infor.RemainResendNum <= 0) {  //超时并且重传次数用光了
+					Log_Output::OutputtoBoth(1, "Can not Recieve Response for packet!");
+					return -1;
+				}
+				else if (time(nullptr) >= Connection_Infor.ResendTimer) {   //超时了
+					err = SendPacket();
+					if (err <= 0) {
+						Log_Output::OutputtoBoth(1, "Send Packet Error!");
 						return -1;
 					}
-					else if (time(nullptr) >= Connection_Infor.ResendTimer) {   //超时了
-						err = SendPacket();
-						if (err <= 0) {
-							Log_Output::OutputtoBoth(1, "Send Packet Error!");
-							return -1;
-						}
-						Connection_Infor.ResendTimer = time(NULL) + DefTimeOut;
-						Connection_Infor.RemainResendNum--;
-					}
-					Sleep(DefSleepTime);   //睡眠一会儿，等待接收
-					continue;
-				}
-				RecivePkt.setPacketLen(err);		//设置接收包长度
-				uint16 op = RecivePkt.CheckPacket();	//检测接收包
-				if (op == 0) {
-					Log_Output::OutputtoBoth(3, "Recive a packet which is illegal!");
 					Connection_Infor.ResendTimer = time(NULL) + DefTimeOut;
-					Sleep(DefSleepTime);   //睡眠一会儿，等待接收
-					continue;
-				} 
-				else if (op == OpERROR) {
-					Parse_ErrorPackets(Now_Blkno);
-					return -2;
+					Connection_Infor.RemainResendNum--;
 				}
-				else if()
-				else if (op == OpACK) {
-					uint16 blk = recvPkt.getBlkNo();	//获取数据块编号
-					if (blk == blkNo) return 1;		//若为当前新的数据块则返回
-					//若为之前的数据块丢掉并重置计时器
-					Connection_Infor.ResendTimer = time(NULL) + DefTimeOut;
-					Sleep(DefSleepTime);   //睡眠一会儿，等待接收
-					continue;
-				}
-				//若为接收文件且受到的为数据包则直接返回
-				else if (!isSender && op == OpDATA) {
-					return 1;
-				}
-				//受到错误包
-				else if (op == OpERROR) {
-					Logger::ost << "Send packet " << recvPkt.getBlkNo()
-						<< " got error-packet  code:" << recvPkt.getErrCode();
-					if (rl > 4)
-						Logger::ost << " message:" << recvPkt.getErrMsg();
-					Logger::ost << ".\n";
-					Logger::logshow(LogERR);
-					return -2;
-				}
-				//收到过短的数据包
-				else if (op == 0) {
-					Logger::ost << "Send packet " << recvPkt.getBlkNo()
-						<< " got packet too short.\n";
-					Logger::logshow(LogERR);
-					sendERROR(4);	//发送错误包
-					return -2;
-				}
-				//收到其他类型的数据包
-				else {
-					Logger::ost << "Send packet " << recvPkt.getBlkNo()
-						<< " got mistake packet.\n";
-					Logger::logshow(LogERR);
-					sendERROR(4);	//发送错误包
-					return -2;
-				}
+				Sleep(DefSleepTime);   //睡眠一会儿，等待接收
+				continue;
 			}
-			return -2;
+			RecivePkt.setPacketLen(err);		//设置接收包长度
+			uint16 op = RecivePkt.CheckPacket();	//检测接收包
+			if (op == 0) {
+				Log_Output::OutputtoBoth(3, "Recive a packet which is illegal!");
+				Connection_Infor.ResendTimer = time(NULL) + (DefTimeOut>>1);  //因为接受的包有问题，所以不能完全等同于一个正确的顺序错误的包，因此采用这种方式
+				Sleep(DefSleepTime);   //睡眠一会儿，等待接收
+				continue;
+			} 
+			else if (op == OpERROR) {
+				Parse_ErrorPackets(Now_Blkno);
+				return -2;
+			}
+			else if (op == OpACK) {
+				uint16 Recived_blk = RecivePkt.ExtractBlockNo();	//获取数据块编号
+				if (Recived_blk == Now_Blkno) {
+					Connection_Infor.SuccessBytes += SendPkt.getDataLen() - 4; //增加传输成功的字节数
+					return 1;		//若为当前新的数据块则返回
+				}
+				//若为之前的数据块丢掉并重置计时器
+				Connection_Infor.ResendTimer = time(NULL) + DefTimeOut;
+				Sleep(DefSleepTime);   //睡眠一会儿，等待接收
+				continue;
+			}
+			else {
+				Log_Output::OutputtoBoth(3, "Recive a packet which is wrong!");
+				Connection_Infor.ResendTimer = time(NULL) + (DefTimeOut >> 1);
+				Sleep(DefSleepTime);   //睡眠一会儿，等待接收
+				continue;
+			}
 		}
-
 	}
 	int Client_Class::Make_Connection() {
 		AssemblyRQPacket();
